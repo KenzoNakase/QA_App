f11f2e8030229965e9f6d737da3738330b02225e
kenzo <kenzonakase@gmail.com>
2018-03-21 21:19:47 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kenzou/nakase/qa_app/QuestionDetailActivity.java
@@ -0,0 +1,141 @@
+package jp.techacademy.kenzou.nakase.qa_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Button;
+import android.support.v4.content.res.ResourcesCompat;
+import android.graphics.drawable.Drawable;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class QuestionDetailActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private Question mQuestion;
+    private QuestionDetailListAdapter mAdapter;
+
+    private DatabaseReference mAnswerRef;
+
+    private ChildEventListener mEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            HashMap map = (HashMap) dataSnapshot.getValue();
+
+            String answerUid = dataSnapshot.getKey();
+
+            for(Answer answer : mQuestion.getAnswers()) {
+                // 同じAnswerUidのものが存在しているときは何もしない
+                if (answerUid.equals(answer.getAnswerUid())) {
+                    return;
+                }
+            }
+
+            String body = (String) map.get("body");
+            String name = (String) map.get("name");
+            String uid = (String) map.get("uid");
+
+            Answer answer = new Answer(body, name, uid, answerUid);
+            mQuestion.getAnswers().add(answer);
+            mAdapter.notifyDataSetChanged();
+
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_question_detail);
+
+        // 渡ってきたQuestionのオブジェクトを保持する
+        Bundle extras = getIntent().getExtras();
+        mQuestion = (Question) extras.get("question");
+
+        setTitle(mQuestion.getTitle());
+
+        // ListViewの準備
+        mListView = (ListView) findViewById(R.id.listView);
+        mAdapter = new QuestionDetailListAdapter(this, mQuestion);
+        mListView.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+
+
+        // 要件1. ログインしている場合に質問詳細画面に「お気に入り」ボタンを表示
+        final Button button = (Button) findViewById(R.id.button);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            button.setVisibility(View.GONE);
+        }
+
+        // 要件2. ログインしている場合に「お気に入り」が既にタップされているかどうかをボタンの見た目で判断できるようにする
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //リソースからボタンのリソースを取得
+                Button btn = (Button)findViewById(R.id.button);
+                //リソースから作成したDrawableのリソースを取得
+                Drawable btn_color = ResourcesCompat.getDrawable(getResources(), R.drawable.color_button, null);
+                //ボタンにDrawableを適用する
+                btn.setBackground(btn_color);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ログイン済みのユーザーを取得する
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (user == null) {
+                    // ログインしていなければログイン画面に遷移させる
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Questionを渡して回答作成画面を起動する
+                    // --- ここから ---
+                    Intent intent = new Intent(getApplicationContext(), AnswerSendActivity.class);
+                    intent.putExtra("question", mQuestion);
+                    startActivity(intent);
+                    // --- ここまで ---
+                }
+            }
+        });
+
+        DatabaseReference dataBaseReference = FirebaseDatabase.getInstance().getReference();
+        mAnswerRef = dataBaseReference.child(Const.ContentsPATH).child(String.valueOf(mQuestion.getGenre())).child(mQuestion.getQuestionUid()).child(Const.AnswersPATH);
+        mAnswerRef.addChildEventListener(mEventListener);
+    }
+}
